17d7519e3639a4a46b4a60064651481d36ea6d0d
Raphael Thomazella <thomazella9@gmail.com>
2018-09-22 15:39:22 -0300

    Add a build step

---
+++ build.bash
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+{
+  echo "\
+#! /usr/bin/env bash
+
+##################################
+# These are dependencies bundled in.
+# Script starts around line 400.
+#################################
+"
+  gsed -Ene 2,9999p <utils.bash
+  echo
+  gsed -Ene 2,9999p <deps/optar.sh
+  echo
+  gsed -Ene 2,9999p <deps/progress.sh
+  echo
+  gsed -Ene 12,9999p <main.bash
+} >dist/main.bash
--- dist/main.bash
+++ dist/main.bash
@@ -1,13 +1,374 @@
 #! /usr/bin/env bash
-# shellcheck disable=SC1090 disable=SC2154
 
-# exit on errors
+##################################
+# These are dependencies bundled in.
+# Script starts around line 370.
+#################################
+
+
 set -e
 
-for name in ./deps/optar.sh ./deps/progress.sh utils.bash; do
-  # shellcheck disable=SC1090
-  source "$name" || bailout "Dependency $name failed. Try cloning the repo?\\n"
+HUE=$((RANDOM % 56 + 164))
+
+bailout() {
+  local message=$*
+  if [[ "$#" == "0" ]]; then
+    message="error"
+  fi
+  echo -ne "\\e[1;31m❌\\040 $message\\e[0m"
+  if [[ ! "$-" =~ i ]]; then
+    exit 1
+  fi
+}
+
+cleanup() {
+  unset N_VERSION LINES INDEX line_version few_version version test_fail DIR_NAME ZIP_NAME PAD HUE
+}
+
+precho() {
+  printf "\\033[38;05;251m"
+  echo -e "${PAD}$*"
+  printf "\\033[0m"
+}
+
+color() {
+  printf "\\033[3;38;05;%sm" "$HUE"
+  echo -ne "$*"
+  printf "\\033[0m"
+}
+
+filesize() {
+  echo -en "$(du -k "$*" | cut -f1)"
+}
+
+isDecimal() {
+  [[ "$*" =~ ^[[:digit:]]+$ ]] && return
+  return 1
+}
+
+clearAndPause() {
+  read -rt 2 || true
+  clear
+}
+
+pause() {
+  precho "\\n"
+  read -rt 2 || true
+}
+
+repliedYes() {
+  if [ "$REPLY" == "y" ] || [ "$REPLY" == "yes" ] || [ "$REPLY" == "Y" ] || [ "$REPLY" == "YES" ]; then
+    return 0
+  fi
+  return 1
+}
+
+printTools() {
+  precho <<<"
+  ### Unix/macOS
+    #### Prerequisites
+
+    * gcc and g++ 4.9.4 or newer, or
+    * clang and clang++ 3.4.2 or newer (macOS: latest Xcode Command Line Tools)
+    * Python 2.6 or 2.7
+    * GNU Make 3.81 or newer
+
+    On macOS, you will need to install the 'Xcode Command Line Tools' by running
+    'xcode-select --install'. This step will install 'clang', 'clang++', and
+    'make'.
+  "
+  precho "See '${PWD}/BUILDING.md'"
+}
+
+isMac(){
+  [[ "$(uname -s)" =~ Darwin ]] && return 0
+  return 1
+}
+
+isUnix(){
+  [[ "$(uname -s)" =~ Darwin ]] || [[ "$(uname -s)" =~ Linux ]] && return 0
+  return 1
+}
+
+isWindows(){
+  [[ ! "$(uname -s)" =~ Darwin ]] && [[ ! "$(uname -s)" =~ Linux ]] && return 0
+  return 1
+}
+
+parse-options() {
+#input   - $@ or string containing shorts (-s), longs (--longs), and arguments
+#returns - arrays with parsed data and opts set as vars
+#exports a var for each option. (-s => $s, --foo => $foo, --long-opt => $long_opt)
+#"-" are translated i(nto "_"
+#"--" signals the end of options
+#shorts take no arguments, to give args to an option use a --long=arg
+
+if [[ "$1" == "--debug" ]]; then
+  echo -e "\\n\\e[4;33m$(printf %${COLUMNS}s) $(center DEBUGGING "${FUNCNAME[0]}"!)$(printf %${COLUMNS}s)\\e[0m\\n"
+  set -x
+fi
+
+if [[ "$#" == 0 ]]; then
+  return
+fi
+
+# Opts we may have inherited from a parent function also using parse-options. Unset to void collisions.
+if [ "$allOptions" ]; then
+  for opt in "${allOptions[@]}"; do
+    unset "$opt"
+  done
+fi
+
+local argn long short noMoreOptions
+
+#echo to split quoted args, repeat until no args left
+for arg in $(echo "$@"); do
+  argn=$((argn + 1))
+
+  # if flag set
+  if [[ "$noMoreOptions" ]]; then
+    #end of options seen, just push remaining args
+    arguments+=("$arg")
+    continue
+  fi
+
+  # if end of options is seen
+  if [[ "$arg" =~ ^--$ ]]; then
+    # set flag to stop parsing
+    noMoreOptions="true"
+    continue
+  fi
+
+  # if long
+  if [[ "$arg" =~ ^--[[:alnum:]] ]]; then
+    #start on char 2, skip leading --
+    long=${arg:2}
+    # substitute any - with _
+    long=${long/-/_}
+    # if opt has an =, it means it has an arg
+    if [[ "$arg" =~ ^--[[:alnum:]][[:alnum:]]+= ]]; then
+      # split opt from arg. Ann=choco makes export ann=choco
+      export ${long%=*}="${long#*=}"
+      longsWithArgs+=(${long%=*})
+    else
+      #no arg, just push
+      longs+=($long)
+    fi
+    continue
+  fi
+
+  # if short
+  if [[ "$arg" =~ ^-[[:alnum:]] ]]; then
+    local i=1 #start on 1, skip leading -
+    # since shorts can be chained (-gpH), look at one char at a time
+    while [ $i != ${#arg} ]; do
+      short=${arg:$i:1}
+        shorts+=($short)
+      i=$((i + 1))
+    done
+    continue
+  fi
+
+  # not a long or short, push as an arg
+  arguments+=($arg)
 done
+
+# give opts with no arguments value "true"
+for short in ${shorts[@]}; do
+  export $short="true"
+done
+
+for long in ${longs[@]}; do
+  export $long="true"
+done
+
+export allOptions="$(get-shorts)$(get-longs)"
+}
+
+#part of parse-options
+get-shorts() {
+  if [ "$shorts" ]; then
+    for short in ${shorts[@]}; do
+      echo -ne "$short "
+    done
+  fi
+}
+
+#part of parse-options
+get-longs() {
+  if [ "$longs" ]; then
+    for long in ${longs[@]}; do
+      echo -ne "$long "
+    done
+  fi
+  if [ "$longsWithArgs" ]; then
+    for long in ${longsWithArgs[@]}; do
+      echo -ne "${long}* "
+    done
+  fi
+}
+
+#part of parse-options
+get-arguments() {
+  for arg in ${arguments[@]}; do
+    echo -ne "$arg "
+  done
+}
+
+get-optionsWithArgs() {
+  for opt in ${shortsWithArgs[@]}; do
+    echo -ne "${opt}* "
+  done
+  for opt in ${longsWithArgs[@]}; do
+    echo -ne "${opt}* "
+  done
+  printf "\\n"
+}
+
+function _eraseLine() {
+  printf "\\r%${progress_line_length}s"
+}
+
+function _printMessageWithRunner() {
+  printf "\\r%0s%s%s%${time_padding}s\\e[33m" '' "$progress_message" "$accumulated_runner" ''
+}
+
+function _show_longrunner() {
+  local pad_right=$progress_pad_left
+  local pos=1
+  while :; do
+    printf "\\r%${progress_pad_left}s%s%${pos}s%s" '' "$progress_message" '' "$progress_runner"
+    pos=$((pos + 1))
+    sleep "$progress_speed"
+    if [[ "$pos" == "$pad_right" ]]; then
+      pos=0
+      _eraseLine
+    fi
+  done
+}
+
+function _show() {
+  local accumulated_runner pad_right right_offset time_padding i
+  accumulated_runner=$progress_runner
+  pad_right=$progress_pad_left
+  right_offset=$((progress_line_length / 10))
+  i=0
+  SECONDS=0
+
+  while :; do
+    time_padding=$((progress_line_length - ${#progress_message} - ${#accumulated_runner} - 5 - right_offset))
+    _printMessageWithRunner
+    _print_time $SECONDS
+    printf "\\e[0m"
+    i=$((i + 1))
+    accumulated_runner=$accumulated_runner$progress_runner
+    sleep "$progress_speed"
+    if [[ "$i" == "$((pad_right / 2))" ]]; then
+      i=0
+      accumulated_runner=$progress_runner
+      _eraseLine
+    fi
+  done
+}
+
+function _print() {
+  local time_padding=$((progress_line_length - progress_pad_left - ${#progress_message} - ${#accumulated_runner} - 5))
+  _printMessageWithRunner
+  printf "     "
+  printf "\\e[0m"
+}
+
+function progress() {
+  case "$1" in
+  "start")
+    if [[ "$progress_pid" ]]; then
+      printf "\\r%2s\\e[31m✘ %b\\n" '' "Progress is already running. Kill it with \\e[100;37mkill $progress_pid\\e[0m"
+      exit 1
+    fi
+    shift
+    progress_speed=0.075 #0.050 good for _show_longrunner
+    progress_runner="."
+    progress_message="$*"
+    progress_line_length=$(tput cols)
+    progress_pad_left=$(((progress_line_length - ${#progress_message} - ${#progress_runner}) / 2))
+    _show &
+    #removes last job put to bg from shell list. Now when killed, it says nothing to stdout.
+    disown
+    progress_pid="$!"
+    ;;
+  "finish")
+    # hard to find a good value here. ${#accumulated_runner} can't be referenced because _show is called with &
+    local time_padding=$(((progress_pad_left / 2) + ${#progress_message} + 4)) # +4: the emoji (3) + 1 space
+
+    # echo time_padding $time_padding >foo.txt
+    # echo progress_line_length "$progress_line_length" >>foo.txt
+    # echo progress_pad_left $progress_pad_left >>foo.txt
+    # echo progress_message ${#progress_message} >>foo.txt
+
+    printf "\\r%${time_padding}s" '' #erases line from beginning to the clock
+    if [ "${#progress_pid}" == "0" ]; then
+      true # noop
+    else
+      kill "$progress_pid"
+    fi
+    progress_pad_left=$((progress_pad_left - 2))
+    case "$2" in
+    # will reprint the message with color indicating the status
+    "0") #success
+      printf "\\r%0s\\e[32m✔ %s\\e[0m\\n" '' "$progress_message"
+      ;;
+    "1") #failure
+      printf "\\r%0s\\e[31m✘ %s\\e[0m\\n" '' "$progress_message"
+      ;;
+    "*") #other status, or no status passed
+      printf "\\r%0s\\e[33m● %s\\e[0m\\n" '' "$progress_message"
+      ;;
+    esac
+    unset progress_speed progress_runner progress_message progress_pid progress_line_length progress_pad_left
+    ;;
+  "total")
+    shift
+    progress_message="Total "
+    local progress_message2=''
+    if [[ -f "$1" ]]; then
+      _print_time "$SECONDS" >>"$1"
+      progress_message="Total✝ "
+    fi
+    progress_line_length=$(tput cols)
+    progress_pad_left=$((progress_line_length - ${#progress_message} - 5))
+    printf "%0s\\e[33;1m%s%s%s\\e[0m\\n" '' "$progress_message" "$(_print_time $SECONDS)" "$progress_message2"
+    ;;
+  "print")
+    shift
+    progress_message="$*"
+    progress_line_length=$(tput cols)
+    progress_pad_left=$(((progress_line_length - ${#progress_message} - ${#progress_runner}) / 2))
+    _print
+    ;;
+  "error")
+    local last="$?"
+    if [ "$last" == 0 ]; then
+      last=1
+    fi
+    shift
+    progress finish "$last"
+    ;;
+  "die")
+    progress error
+    [[ ! "$-" =~ i ]] && exit 1
+    return 1
+    ;;
+  *)
+    echo "Please give a valid command"
+    echo "start, finish, total or print"
+    ;;
+  esac
+}
+
+function _print_time() { #$1 - a number in seconds
+  if [[ "$#" == 0 ]]; then exit 1; fi
+  printf "%02d:%02d" "$(($1 / 60))" "$(($1 % 60))"
+}
 
 ##############################
 ##########  SETUP  ###########
